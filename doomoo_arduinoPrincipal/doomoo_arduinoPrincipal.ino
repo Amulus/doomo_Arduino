ab763d3226a24c786aad0d90a107861177a056ac
Amulus <axel.murat@laposte.net>
2017-02-14 18:50:02 +0100

    doomo

---
+++ doomoo_arduinoPrincipal/doomoo_arduinoPrincipal.ino
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+#DEFINE PORT 9600
+
+const SoftwareSerial portRaspbery(1, 2);
+
+const SoftwareSerial portModule1(3, 4);
+const SoftwareSerial portModule2(4, 5);
+const SoftwareSerial portModule3(6, 7);
+const SoftwareSerial portModule4(8, 9);
+const SoftwareSerial portModule5(10, 11);
+
+
+
+/*
+  Turns on an LED on for one second, then off for one second, repeatedly.
+*/
+
+
+void setup()
+{
+    /*
+        Initialize serial port
+    */
+	Serial.begin(PORT);
+    portRaspbery.begin(PORT);
+    portModule1.begin(PORT);
+    portModule2.begin(PORT);
+    portModule3.begin(PORT);
+    portModule4.begin(PORT);
+    portModule5.begin(PORT);
+
+}
+
+void loop()
+{
+
+}
